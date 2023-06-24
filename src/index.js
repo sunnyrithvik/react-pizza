26b60583c96cfacea4ba5e4da08c6ab7542d1323
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-06-24 13:15:02 +0530

    Initial files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import pizzaData from "./data.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>fast React Pizza Com</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+      {/* <Pizza
+        name="pizza1"
+        ingredients="tomato, chilli"
+        photo="pizzas/spinaci.jpg"
+        price="10"
+      />
+      <Pizza
+        name="pizza1"
+        ingredients="tomato, chilli"
+        photo="pizzas/spinaci.jpg"
+        price="13"
+      /> */}
+    </main>
+  );
+}
+
+function Pizza(props) {
+  // console.log(props);
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 8;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <div className="order">
+          <p>we are open until {closeHour}:00. Order online</p>
+          <button className="btn">Order Now</button>
+        </div>
+      ) : null}
+
+      {/* <p>
+        {new Date().toLocaleTimeString()} {hour} we are currently open
+      </p> */}
+    </footer>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
